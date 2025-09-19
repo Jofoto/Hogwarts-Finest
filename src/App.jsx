a95ee6045b31b5a0cdd93dea6ae60f16e818bb63
Prüger <kprueger@ad.esi.adp.com>
2025-09-19 12:05:03 +0200

    Fixed list return

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,28 @@ import './App.css'
 
 function App() {
   const customerList = [
-    {id: 1, name: "Sam Smith", email: "samsmith@adp.com", password: "sam1234"}
+    { id: 1, name: "Sam Smith", email: "samsmith@adp.com", password: "sam1234" }
   ];
 
   const [customers, setCustomers] = useState(customerList);
 
-  return(
+  return (
     <div className='app'>
       <header><h1>Hogwarts Finest Customer List</h1></header>
-        <ul className='customerList'>
-          {customerList.map((customers) => {
-            return 
-              <li key={customers.id}>
-                {customers.name},
-                {customers.email},
-                {customers.password}
-              </li>
-              console.log(`${customerList[0].name} Here are the customers!`);
-          
-            })
-          }
-        </ul>
-      
+      <ul className='customerList'>
+        {customerList.map((customers) => {
+          return (
+            <li key={customers.id}>
+              {customers.name},
+              {customers.email},
+              {customers.password}
+            </li>
+          );
+          console.log(`${customerList[0].name} Here are the customers!`);
+        })
+        }
+      </ul>
+
     </div>
   )
 }
