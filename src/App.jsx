dd4df33cf96fd7b5676926b3dbd3d9a1ba5ceb27
Fotopoulou <gfotopou@ad.esi.adp.com>
2025-09-19 11:59:56 +0200

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const customerList = [
+    {id: 1, name: "Sam Smith", email: "samsmith@adp.com", password: "sam1234"}
+  ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const [customers, setCustomers] = useState(customerList);
+
+  return(
+    <div className='app'>
+      <header><h1>Hogwarts Finest Customer List</h1></header>
+        <ul className='customerList'>
+          {customerList.map((customers) => {
+            return 
+              <li key={customers.id}>
+                {customers.name},
+                {customers.email},
+                {customers.password}
+              </li>
+              console.log(`${customerList[0].name} Here are the customers!`);
+          
+            })
+          }
+        </ul>
+      
+    </div>
   )
 }
 
