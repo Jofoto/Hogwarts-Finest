a5a1bbeb1ebf6e0e123ca29ddc84205c58b01880
kprueger-work <kprueger@ad.esi.adp.com>
2025-09-24 12:41:49 +0200

    Update react-router import to react-router-dom import

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Route, Routes, Navigate, Link, NavLink } from 'react-router'
+import { Route, Routes, Navigate, NavLink } from 'react-router-dom';
 
 import Home from './content/Home.jsx';
 import Add from './content/Add.jsx';
